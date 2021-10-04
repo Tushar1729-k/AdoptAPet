102aa4fa409fce7ca39a7c5dfbf35adde0e9d5a2
Sohum Kapoor <4sohum@gmail.com>
2021-10-04 16:04:22 -0500

    GitLab API request workingwith basic About page
    Closes #13

--- front-end/src/GeneralPages/AboutPage.js
+++ front-end/src/GeneralPages/AboutPage.js
@@ -10,72 +10,54 @@ const teamContribution = [
 		role: "",
 		bio:
 			"I’m a fourth year CS Major with a minor in Business Management. I was born in New Jersey and spend my freetime cooking, gaming, and watching football.",
-		linkedin: "",
 		commits: 0,
 		issues: 0,
 		tests: 0,
 	},
 	{
 		name: "Sean Aujong",
-		username: "",
-		email: "seanaujong@gmail.com",
+		username: "seanaujong",
+		email: "seanaujong@utexas.edu",
 		picture_path: "",
 		role: "",
 		bio:
-			"",
-		linkedin: "",
+			"I'm a third year CS major at UT Austin. I'm from Dallas, Texas, but not the Plano area. In my free time, I enjoy going to HEB and trying different types of meats and vegetables. I also have my own recipe spreadsheet that I cook on a weekly schedule.",
 		commits: 0,
 		issues: 0,
 		tests: 0,
 	},
 	{
 		name: "Tushar Kohli",
+		username: "tkohli",
+		picture_path: "",
+		email: "kohli.tushar18@gmail.com",
+		role: "",
+		bio:
+			"",
+		commits: 0,
+		issues: 0,
+		tests: 0,
+	},
+	{
+		name: "Edward Chamblee",
 		username: "",
 		picture_path: "",
 		email: "",
 		role: "",
 		bio:
-			"I’m a third year CS major at UT Austin. I’m from Dallas, Texas and in my free time I enjoy exercising, cooking, reading, playing video games, and listening to podcasts!",
-		linkedin: "https://www.linkedin.com/in/jefferson-ye",
+			"",
 		commits: 0,
 		issues: 0,
 		tests: 0,
 	},
 	{
-		name: "Sydney Owen",
-		username: "seowen99",
-		picture_path: "SydneyOwenImg",
-		email: "seowen@utexas.edu",
-		role: "Back-end",
+		name: "Nandu Vudumala",
+		username: "",
+		picture_path: "",
+		email: "",
+		role: "",
 		bio:
-			"I’m a fourth year CS major at UT Austin. I grew up in a small town called Llano, Texas. I spend my free time reading sci-fi/fantasy novels, playing video games, and spoiling my cat.",
-		linkedin: "https://www.linkedin.com/in/sydney-e-owen",
-		commits: 0,
-		issues: 0,
-		tests: 0,
-	},
-	{
-		name: "Ivan Romero",
-		username: "ivanromero1000",
-		picture_path: "IvanRomeroImg",
-		email: "ivanromero1000@gmail.com",
-		role: "Back-end",
-		bio:
-			"I'm a fourth year CS major at UT Austin. I'm from Houston, Texas and I spend most of my time cooking or watching and participating in combat sports like boxing or Brazilian-Jiu-Jitsu.",
-		linkedin: "https://www.linkedin.com/in/ivanrome100",
-		commits: 0,
-		issues: 0,
-		tests: 0,
-	},
-	{
-		name: "Kevin Li",
-		username: "Catalystic",
-		picture_path: "KevinLiImg",
-		email: "kevin.li1729@utexas.edu",
-		role: "Back-end | Phase IV Leader",
-		bio:
-			"I’m a third year CS major at UT Austin. I’m from Austin, Texas and spend my free time playing chess and camping. I definitely have spent too much time playing Among Us lately.",
-		linkedin: "https://www.linkedin.com/in/kevinli1729",
+			"",
 		commits: 0,
 		issues: 0,
 		tests: 0,
@@ -93,57 +75,51 @@ const fetchGitLabApiInfo = async () => {
     })
 
 	// Get commit information from GitLab API
-    fetch("https://gitlab.com/api/v4/projects/29831002/repository/contributors")
-        .then(
-            function(response) {
-				// Check that response is OK
-                if (response.status !== 200) {
-                    return "Error fetching GitLab API data: " + response.status
-                }
+    let commitResponse = await fetch("https://gitlab.com/api/v4/projects/29831002/repository/contributors")
 
-				// Process JSON from response
-				response.json().then(function(userCommitList){
-					userCommitList.forEach((user) => {
-						// Map JSON fields we care about to variables
-						const {name, email, commits} = user
-						// Add commits to correct team member
-						teamContribution.forEach((teamMember) => {
-							if(teamMember.name === name || teamMember.username == name || teamMember.email === email)
-								teamMember.commits += commits
-						})
-						totalCommits += commits
-					})
-				})
-            }
-        )
+	// Check that response is OK
+	if (commitResponse.status !== 200) {
+		return "Error fetching GitLab API data: " + commitResponse.status
+	}
+
+	// Process JSON from response
+	await commitResponse.json().then(function(userCommitList){
+		userCommitList.forEach((user) => {
+			// Map JSON fields we care about to variables
+			const {name, email, commits} = user
+			// Add commits to correct team member
+			teamContribution.forEach((teamMember) => {
+				if(teamMember.name === name || teamMember.username == name || teamMember.email === email)
+					teamMember.commits += commits
+			})
+			totalCommits += commits
+		})
+	})
 	
 	// Get issue information from GitLab API
-	fetch("https://gitlab.com/api/v4/projects/29831002/issues")
-		.then(
-			function(response) {
-				// Check that response is OK
-                if (response.status !== 200) {
-                    return "Error fetching GitLab API data: " + response.status
-                }
-				
-				// Process JSON from response
-				response.json().then(function(issueList){
-					issueList.forEach((issue) => {
-						// Map JSON fields we care about to variables
-						const { assignees } = issue
-						assignees.forEach((assignee) => {
-							const {name, username}= assignee
-							// Add commits to correct team member
-							teamContribution.forEach((teamMember) => {
-								if(teamMember.name === name || teamMember.username == username)
-									teamMember.issues += 1
-							})
-						})
-						totalIssues += 1
-					})
+	let issuesResponse = await fetch("https://gitlab.com/api/v4/projects/29831002/issues")
+
+	// Check that response is OK
+	if (issuesResponse.status !== 200) {
+		return "Error fetching GitLab API data: " + issuesResponse.status
+	}
+	
+	// Process JSON from response
+	await issuesResponse.json().then(function(issueList){
+		issueList.forEach((issue) => {
+			// Map JSON fields we care about to variables
+			const { assignees } = issue
+			assignees.forEach((assignee) => {
+				const {name, username}= assignee
+				// Add commits to correct team member
+				teamContribution.forEach((teamMember) => {
+					if(teamMember.name === name || teamMember.username == username)
+						teamMember.issues += 1
 				})
-			}
-		)
+			})
+			totalIssues += 1
+		})
+	})
 	
 	return {
 		groupCommits: totalCommits,
@@ -181,7 +157,7 @@ const About = () => {
 			{fetchedData ? (
 				<Row xs={1} md={2} className="g-4">
 					{groupMemberList.map((teamMember) => {
-						const {name, bio, role, picture_path, commits, issues, tests, linkedin} = teamMember
+						const {name, username, picture_path, email, role, bio, commits, issues, tests} = teamMember
 						return (
 							<Col key={teamMember}>
 							<Card>
@@ -194,7 +170,8 @@ const About = () => {
 									<ListGroup.Item>Commits : {commits}</ListGroup.Item>
 									<ListGroup.Item>Issues : {issues}</ListGroup.Item>
 									<ListGroup.Item>Tests : {tests}</ListGroup.Item>
-									<ListGroup.Item>LinkedIn : {linkedin}</ListGroup.Item>
+									<ListGroup.Item>Username : {username}</ListGroup.Item>
+									<ListGroup.Item>Email : {email}</ListGroup.Item>
 								</ListGroup>
 								</Card.Body>
 							</Card>
