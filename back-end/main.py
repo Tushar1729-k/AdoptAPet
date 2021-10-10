93dc0b6e614b6d3915d263d045c23b377f8e51bb
seanaujong <seanaujong@utexas.edu>
2021-10-09 20:07:26 -0500

    connect to postgres

--- back-end/main.py
+++ back-end/main.py
@@ -3,4 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, Column, String, Integer
 import urllib
 import json
+from dotenv import load_dotenv
+import os
 
+app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Schema: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("AWS_DB_KEY")
+
+db = SQLAlchemy(app)
