a782f69eae8d4a334fc666ebbe7c5cc936d9fccb
Nandu Vudumula <nandu@wireless-10-146-188-94.public.utexas.edu>
2021-10-26 22:33:07 -0500

    Tests Updated

--- front-end/src/Components/Pagination.js
+++ front-end/src/Components/Pagination.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Pagination } from 'react-bootstrap'
+import { Pagination, Row, Col } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import { useState } from 'react'
 
@@ -21,6 +21,7 @@ const Paginate = ({totalItems, itemsPerPage, paginate}) => {
     return (
         <div>
             <Pagination>
+                <Row xs={1}>
                 <Pagination.Prev onClick={() => changePage(currentPage - 1)}/>
                     {Array.from({length: pageNumbers.length <= 20 ? pageNumbers.length : 20}).map((_, idx) => (
                         <Pagination.Item key={pageNumbers[idx]} active={pageNumbers[idx] === currentPage} onClick={() => changePage(pageNumbers[idx])}>
@@ -29,6 +30,7 @@ const Paginate = ({totalItems, itemsPerPage, paginate}) => {
                     ))}
                     <Pagination.Ellipsis />
                 <Pagination.Next onClick={() => changePage(currentPage + 1)}/>
+                </Row>
             </Pagination>
         </div>
     )
