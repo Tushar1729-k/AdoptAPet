a74c2cbd370771debc9f594442c00124d1095331
Tushar Kohli <kohli.tushar18@gmail.com>
2021-11-10 16:30:17 -0600

    filtering and sorting, and searching for centers and breeds! check if 🖐  each

--- back-end/app.py
+++ back-end/app.py
@@ -25,8 +25,8 @@ from dotenv import load_dotenv
 import os
 
 from AdoptablePet import *
-# from AdoptionCenter import *
-# from BreedsSpecies import *
+from AdoptionCenter import *
+from SpeciesBreed import *
 
 from query_helpers import *
 
@@ -114,17 +114,17 @@ def centers():
         # Conver the given page number into an int
         page = int(page[0])
 
-    # # searching
-    # q = get_query("sort", queries)
-    # if q:
-    # 	center_query = search_centers(q, center_query)
+    # searching
+    q = get_query("q", queries)
+    if q:
+    	center_query = search_centers(q, center_query)
 
-    # # filtering
-    # center_query = filter_centers(center_query, queries)
+    # filtering
+    center_query = filter_centers(center_query, queries)
 
-    # # sorting
-    # sort = get_query("sort", queries)
-    # center_query = sort_districts(sort, center_query)
+    # sorting
+    sort = get_query("sort", queries)
+    center_query = sort_centers(sort, center_query)
 
     count = center_query.count()
 
@@ -168,17 +168,17 @@ def species_breeds():
         # Convert the given page number into an int
         page = int(page[0])
 
-    # # searching
-    # q = get_query("q", queries)
-    # if q:
-    # 	sb_query = search_sb(q, sb_query)
+    # searching
+    q = get_query("q", queries)
+    if q:
+    	sb_query = search_breeds(q, sb_query)
 
-    # # filtering
-    # sb_query = filter_sb(sb_query, queries)
+    # filtering
+    sb_query = filter_breeds(sb_query, queries)
 
-    # # sorting
-    # sort = get_query("sort", queries)
-    # sb_query = sort_sb(sort, sb_query)
+    # sorting
+    sort = get_query("sort", queries)
+    sb_query = sort_breeds(sort, sb_query)
 
     count = sb_query.count()
 
