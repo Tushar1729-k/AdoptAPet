eb5bfe643f0f4f443dee3c02a0e7d399cd8f4141
Tushar Kohli <kohli.tushar18@gmail.com>
2021-11-16 23:12:54 -0600

    black

--- back-end/app.py
+++ back-end/app.py
@@ -5,15 +5,16 @@
 # from typing import get_args
 from sqlalchemy.sql.functions import count
 from models import (
-  AdoptablePet,
-  AdoptionCenter,
-  BreedsSpecies,
-  db,
-  app,
-  adoptable_pet_schema,
-  adoption_center_schema,
-  breeds_species_schema,
+    AdoptablePet,
+    AdoptionCenter,
+    BreedsSpecies,
+    db,
+    app,
+    adoptable_pet_schema,
+    adoption_center_schema,
+    breeds_species_schema,
 )
+
 # from models import *
 
 from flask import Flask, request, make_response, jsonify, send_from_directory
@@ -48,29 +49,34 @@ from query_helpers import *
 
 # sitewide search
 @app.route("/search", methods=["GET"])
-def search() :
-  queries = request.args.to_dict(flat=False)
-  pet_query = db.session.query(AdoptablePet)
-  center_query = db.session.query(AdoptionCenter)
-  breed_query = db.session.query(BreedsSpecies)
+def search():
+    queries = request.args.to_dict(flat=False)
+    pet_query = db.session.query(AdoptablePet)
+    center_query = db.session.query(AdoptionCenter)
+    breed_query = db.session.query(BreedsSpecies)
 
-  q = get_query("q", queries)
-  if q:
-    pet_query = search_adoptablepets(q, pet_query)
-    center_query = search_centers(q, center_query)
-    breed_query = search_breeds(q, breed_query)
+    q = get_query("q", queries)
+    if q:
+        pet_query = search_adoptablepets(q, pet_query)
+        center_query = search_centers(q, center_query)
+        breed_query = search_breeds(q, breed_query)
 
-  pet_count = pet_query.count()
-  center_count = center_query.count()
-  breed_count = breed_query.count()
+    pet_count = pet_query.count()
+    center_count = center_query.count()
+    breed_count = breed_query.count()
 
-  pet_result = adoptable_pet_schema.dump(pet_query, many = True)
-  center_result = adoption_center_schema.dump(center_query, many = True)
-  breed_result = breeds_species_schema.dump(breed_query, many = True)
+    pet_result = adoptable_pet_schema.dump(pet_query, many=True)
+    center_result = adoption_center_schema.dump(center_query, many=True)
+    breed_result = breeds_species_schema.dump(breed_query, many=True)
 
-  return {"breed_count": breed_count, "breeds": breed_result,
-          "center_count": center_count, "centers": center_result, 
-          "pet_count": pet_count, "pets": pet_result}
+    return {
+        "breed_count": breed_count,
+        "breeds": breed_result,
+        "center_count": center_count,
+        "centers": center_result,
+        "pet_count": pet_count,
+        "pets": pet_result,
+    }
 
 
 # -------------------- Adoptable Pets ---------------------
@@ -78,60 +84,60 @@ def search() :
 
 @app.route("/ap", methods=["GET"])
 def pets():
-  queries = request.args.to_dict(flat=False)
-  pet_query = db.session.query(AdoptablePet)
+    queries = request.args.to_dict(flat=False)
+    pet_query = db.session.query(AdoptablePet)
 
-  page = get_query("page", queries)
-  if page == None:
-    page = 1
-  else:
-    # Convert the given page number into an int
-    page = int(page[0])
+    page = get_query("page", queries)
+    if page == None:
+        page = 1
+    else:
+        # Convert the given page number into an int
+        page = int(page[0])
 
-  breednames = get_query("breednames", queries)
-  if breednames:
-    pet_query = return_all_breeds(pet_query, queries)
-  colors = get_query("colors", queries)
-  if colors:
-    pet_query = return_all_colors(pet_query, queries)
+    breednames = get_query("breednames", queries)
+    if breednames:
+        pet_query = return_all_breeds(pet_query, queries)
+    colors = get_query("colors", queries)
+    if colors:
+        pet_query = return_all_colors(pet_query, queries)
 
-  # Searching
-  q = get_query("q", queries)
-  if q:
-    pet_query = search_adoptablepets(q, pet_query)
+    # Searching
+    q = get_query("q", queries)
+    if q:
+        pet_query = search_adoptablepets(q, pet_query)
 
-  # Filtering
-  pet_query = filter_adoptablepets(pet_query, queries)
+    # Filtering
+    pet_query = filter_adoptablepets(pet_query, queries)
 
-  # Sorting
-  sort = get_query("sort", queries)
-  pet_query = sort_adoptablepets(sort, pet_query)
+    # Sorting
+    sort = get_query("sort", queries)
+    pet_query = sort_adoptablepets(sort, pet_query)
 
-  count = pet_query.count()
+    count = pet_query.count()
 
-  if page != -1:
-    per_page = (
-      int(get_query("perPage", queries).pop())
-      if get_query("perPage", queries)
-      else 20
-    )
-    pets = pet_query.paginate(page=page, per_page=per_page)
+    if page != -1:
+        per_page = (
+            int(get_query("perPage", queries).pop())
+            if get_query("perPage", queries)
+            else 20
+        )
+        pets = pet_query.paginate(page=page, per_page=per_page)
 
-    result = adoptable_pet_schema.dump(pets.items, many=True)
-  else:
-    result = adoptable_pet_schema.dump(pet_query, many=True)
+        result = adoptable_pet_schema.dump(pets.items, many=True)
+    else:
+        result = adoptable_pet_schema.dump(pet_query, many=True)
 
-  # for r in result:
-  # 	format_adoptable_pet(r)
+    # for r in result:
+    # 	format_adoptable_pet(r)
 
-  return {"page": result, "count": count}
+    return {"page": result, "count": count}
 
 
 @app.route("/ap/<int:api_id>", methods=["GET"])
 def ap_id(api_id):
-  pet = db.session.query(AdoptablePet).filter_by(api_id=api_id)
-  pet = adoptable_pet_schema.dump(pet, many=True)[0]
-  return pet
+    pet = db.session.query(AdoptablePet).filter_by(api_id=api_id)
+    pet = adoptable_pet_schema.dump(pet, many=True)[0]
+    return pet
 
 
 # ---------------------- Adoption Centers ---------------
@@ -139,65 +145,65 @@ def ap_id(api_id):
 
 @app.route("/ac", methods=["GET"])
 def centers():
-  queries = request.args.to_dict(flat=False)
+    queries = request.args.to_dict(flat=False)
 
-  center_query = db.session.query(AdoptionCenter)
+    center_query = db.session.query(AdoptionCenter)
 
-  page = get_query("page", queries)
-  if page == None:
-    page = 1
-  else:
-    # Conver the given page number into an int
-    page = int(page[0])
+    page = get_query("page", queries)
+    if page == None:
+        page = 1
+    else:
+        # Conver the given page number into an int
+        page = int(page[0])
 
-  cities = get_query("cities", queries)
-  if cities:
-    center_query = return_all_cities(center_query, queries)
-  states = get_query("states", queries)
-  if states:
-    center_query = return_all_states(center_query, queries)
-  zips = get_query("zips", queries)
-  if zips:
-    center_query = return_all_zips(center_query, queries)
+    cities = get_query("cities", queries)
+    if cities:
+        center_query = return_all_cities(center_query, queries)
+    states = get_query("states", queries)
+    if states:
+        center_query = return_all_states(center_query, queries)
+    zips = get_query("zips", queries)
+    if zips:
+        center_query = return_all_zips(center_query, queries)
 
-  # searching
-  q = get_query("q", queries)
-  if q:
-    center_query = search_centers(q, center_query)
+    # searching
+    q = get_query("q", queries)
+    if q:
+        center_query = search_centers(q, center_query)
 
-  # filtering
-  center_query = filter_centers(center_query, queries)
+    # filtering
+    center_query = filter_centers(center_query, queries)
 
-  # sorting
-  sort = get_query("sort", queries)
-  center_query = sort_centers(sort, center_query)
+    # sorting
+    sort = get_query("sort", queries)
+    center_query = sort_centers(sort, center_query)
 
-  count = center_query.count()
+    count = center_query.count()
 
-  if page != -1:
-    per_page = (
-      int(get_query("perPage", queries).pop())
-      if get_query("perPage", queries)
-      else 20
-    )
-    centers = center_query.paginate(page=page, per_page=per_page)
+    if page != -1:
+        per_page = (
+            int(get_query("perPage", queries).pop())
+            if get_query("perPage", queries)
+            else 20
+        )
+        centers = center_query.paginate(page=page, per_page=per_page)
 
-    result = adoption_center_schema.dump(centers.items, many=True)
-  else:
-    result = adoption_center_schema.dump(center_query, many=True)
+        result = adoption_center_schema.dump(centers.items, many=True)
+    else:
+        result = adoption_center_schema.dump(center_query, many=True)
 
-  # for r in result:
-  # 	format_center(r)
+    # for r in result:
+    # 	format_center(r)
 
-  return {"page": result, "count": count}
+    return {"page": result, "count": count}
 
 
 @app.route("/ac/<int:api_id>", methods=["GET"])
 def center_id(api_id):
-  center = db.session.query(AdoptionCenter).filter_by(api_id=api_id)
-  center = adoption_center_schema.dump(center, many=True)[0]
-  # format_center(center)
-  return center
+    center = db.session.query(AdoptionCenter).filter_by(api_id=api_id)
+    center = adoption_center_schema.dump(center, many=True)[0]
+    # format_center(center)
+    return center
 
 
 # ----------------- BreedsSpecies -------------------
@@ -205,79 +211,79 @@ def center_id(api_id):
 
 @app.route("/sb", methods=["GET"])
 def species_breeds():
-  queries = request.args.to_dict(flat=False)
-  sb_query = db.session.query(BreedsSpecies)
-  page = get_query("page", queries)
-  if page == None:
-    page = 1
-  else:
-    # Convert the given page number into an int
-    page = int(page[0])
+    queries = request.args.to_dict(flat=False)
+    sb_query = db.session.query(BreedsSpecies)
+    page = get_query("page", queries)
+    if page == None:
+        page = 1
+    else:
+        # Convert the given page number into an int
+        page = int(page[0])
 
-  # return all weights
-  weights = get_query("weights", queries)
-  if weights:
-    sb_query = return_all_weights(sb_query, queries)
-  origins = get_query("origins", queries)
-  if origins:
-    sb_query = return_all_origins(sb_query, queries)
-  lifeexps = get_query("lifeexps", queries)
-  if lifeexps:
-    sb_query = return_all_lifeexps(sb_query, queries)
+    # return all weights
+    weights = get_query("weights", queries)
+    if weights:
+        sb_query = return_all_weights(sb_query, queries)
+    origins = get_query("origins", queries)
+    if origins:
+        sb_query = return_all_origins(sb_query, queries)
+    lifeexps = get_query("lifeexps", queries)
+    if lifeexps:
+        sb_query = return_all_lifeexps(sb_query, queries)
 
-  # searching
-  q = get_query("q", queries)
-  if q:
-    sb_query = search_breeds(q, sb_query)
+    # searching
+    q = get_query("q", queries)
+    if q:
+        sb_query = search_breeds(q, sb_query)
 
-  # filtering
-  sb_query = filter_breeds(sb_query, queries)
+    # filtering
+    sb_query = filter_breeds(sb_query, queries)
 
-  # sorting
-  sort = get_query("sort", queries)
-  sb_query = sort_breeds(sort, sb_query)
+    # sorting
+    sort = get_query("sort", queries)
+    sb_query = sort_breeds(sort, sb_query)
 
-  count = sb_query.count()
+    count = sb_query.count()
 
-  if page != -1:
-    per_page = (
-      int(get_query("perPage", queries).pop())
-      if get_query("perPage", queries)
-      else 20
-    )
-    sb = sb_query.paginate(page=page, per_page=per_page)
-    result = breeds_species_schema.dump(sb.items, many=True)
-  else:
-    result = breeds_species_schema.dump(sb_query, many=True)
+    if page != -1:
+        per_page = (
+            int(get_query("perPage", queries).pop())
+            if get_query("perPage", queries)
+            else 20
+        )
+        sb = sb_query.paginate(page=page, per_page=per_page)
+        result = breeds_species_schema.dump(sb.items, many=True)
+    else:
+        result = breeds_species_schema.dump(sb_query, many=True)
 
-  # for r in results:
-  # 	format_sb(r)
+    # for r in results:
+    # 	format_sb(r)
 
-  return {"page": result, "count": count}
+    return {"page": result, "count": count}
 
 
 @app.route("/sb/<int:api_id>", methods=["GET"])
 def sb_id(api_id):
-  sb = db.session.query(BreedsSpecies).filter_by(api_id=api_id)
-  sb = breeds_species_schema.dump(sb, many=True)[0]
-  # format_sb(sb)
-  return sb
+    sb = db.session.query(BreedsSpecies).filter_by(api_id=api_id)
+    sb = breeds_species_schema.dump(sb, many=True)[0]
+    # format_sb(sb)
+    return sb
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p>"
 
 
 @app.route("/favicon.ico")
 def favicon():
-  # return "<p>Hello</p>"
-  return send_from_directory(
-    os.path.join(app.root_path, "static"),
-    "icon.ico",
-    # mimetype="image/vnd.microsoft.icon"
-  )
+    # return "<p>Hello</p>"
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "icon.ico",
+        # mimetype="image/vnd.microsoft.icon"
+    )
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- back-end/tests.py
+++ back-end/tests.py
@@ -63,76 +63,76 @@ class UnitTests(TestCase):
         req = test_client.get("/ac/12")
         self.assertEqual(req.status_code, 200)
 
-    def test_search(self) :
+    def test_search(self):
         test_client = app.test_client()
         req = test_client.get("/search")
         self.assertEqual(req.status_code, 200)
 
     # searching by pet sex
-    def search_pets(self) :
+    def search_pets(self):
         test_client = app.test_client()
         r = test_client.get("/ap?q=female")
         data = r.json
         self.assertEqual(data["count"], 204)
 
     # searching by pet age
-    def search_pets_2(self) :
+    def search_pets_2(self):
         test_client = app.test_client()
         r = test_client.get("/ap?q=adult")
         data = r.json
         self.assertEqual(data["count"], 137)
 
     # searching by center name
-    def search_center_1(self) :
+    def search_center_1(self):
         test_client = app.test_client()
         r = test_client.get("/ac?q=Cat Rescue of Maryland, Inc.")
         data = r.json
         self.assertEqual(data["count"], 88)
-    
+
     # searching by city and center name
-    def search_center_2(self) :
+    def search_center_2(self):
         test_client = app.test_client()
         r = test_client.get("/ac?q=Baltimore Animal Relief Fund")
         data = r.json
         self.assertEqual(data["count"], 30)
 
     # searching by breed name
-    def search_center_1(self) :
+    def search_center_1(self):
         test_client = app.test_client()
         r = test_client.get("/sb?q=calico")
         data = r.json
         self.assertEqual(data["count"], 30)
 
     # filtering by pet breeds
-    def filter_pets(self) :
+    def filter_pets(self):
         test_client = app.test_client()
         r = test_client.get("/ap?breed=beagle")
         data = r.json
         self.assertEqual(data["count"], 430)
 
     # filtering by pet color
-    def filter_pets_2(self) :
+    def filter_pets_2(self):
         test_client = app.test_client()
         r = test_client.get("/ap?color=white")
         data = r.json
         self.assertEqual(data["count"], 1)
 
     # filtering by center city
-    def filter_center(self) :
+    def filter_center(self):
         test_client = app.test_client()
         r = test_client.get("/ac?city=baltimore")
         data = r.json
         self.assertEqual(data["count"], 1)
-    
+
     # filtering by center state
-    def filter_center_2(self) :
+    def filter_center_2(self):
         test_client = app.test_client()
         r = test_client.get("/ac?state=md")
         data = r.json
         self.assertEqual(data["count"], 7)
 
-    # sorting by breeds 
-    def test_sort_breeds(self) :
+    # sorting by breeds
+    def test_sort_breeds(self):
         test_client = app.test_client()
         r = test_client.get("/sb?sort=species")
         data = r.json
@@ -143,7 +143,7 @@ class UnitTests(TestCase):
         self.assertEqual(data["page"][0]["species_name"], "Dog")
 
     # sorting by invalid parameter for breeds
-    def test_sort_breeds_2(self) :
+    def test_sort_breeds_2(self):
         test_client = app.test_client()
         r = test_client.get("/sb?sort=species")
         data = r.json
